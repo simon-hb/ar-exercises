eb33dc4352b96496830aefaed27165de935072b8
Simon Jung <simon.hb.jung@hotmail.com>
2020-08-18 17:21:06 +0000

    fixed exercise 4, done 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -12,10 +12,10 @@ surrey = Store.create(name: "Surrey", annual_revenue: "224000", mens_apparel: fa
 whistler = Store.create(name: "Whistler", annual_revenue: "1900000", mens_apparel: true, womens_apparel: false)
 yaletown = Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
 
-@mens_stores = Stores.where(mens_apparel: true)
+@mens_stores = Store.where(mens_apparel: true)
 
-@mens_store.each { |x| puts "name: #{x.name} annual_revenue: #{x.annual_revenue}"}
+@mens_stores.each { |x| puts "name: #{x.name} annual_revenue: #{x.annual_revenue}"}
 
-@womens_store_under_mil = Stores.where(womens_apparel: true).where(annual_revenue < 1000000)
+@womens_store_under_mil = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
 
 @womens_store_under_mil.each { |x| puts "name: #{x.name} annual_revenue: #{x.annual_revenue}"}
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+#could put the code directly in #{} but assigning variables is cleaner
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue: $#{total_revenue}"
+
+avg_revenue = total_revenue/Store.count
+puts "Average revenue: #{avg_revenue}"
+
+stores_greater_mil = Store.where('annual_revenue > 1000000').count
+puts "Stores with > 1 mil revenue: #{stores_greater_mil}"
+
