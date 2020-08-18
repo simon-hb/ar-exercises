c27e9190d5a1614c9ddd9201dd778da4f8331e69
Simon Jung <simon.hb.jung@hotmail.com>
2020-08-18 16:34:30 +0000

    store 1 name updated

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+#loads first store in db
+@store1 = Store.first
+#searches db and returns first user with find_by query
+@store2 = Store.find_by(id: 2)
+@store1.name = 'gap'
+@store1.save
